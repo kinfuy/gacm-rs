8095c83448b52f376ad69bdbeeee7c8eedae0aa4
kinfuy <yonghu520@outlook.com>
2023-04-15 22:41:55 +0800

    fix: detail

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,57 @@
-pub mod command {
-    use structopt::StructOpt;
+mod config;
 
-    #[derive(StructOpt, Debug)]
-    #[structopt(name = "gacm")]
-    pub struct Cli {
-        pub pattern: String,
-        #[structopt(parse(from_os_str))]
-        pub command: std::path::PathBuf,
-    }
+use clap::{arg, Command};
+use std::path::PathBuf;
+
+pub fn push_args() -> Vec<clap::Arg> {
+    vec![arg!(-m --message <MESSAGE>)]
+}
+
+pub fn cli() -> Command {
+    Command::new("gacm")
+        .about("Git Account Management Tools & NPM Registry Manager")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(true)
+        .subcommand(
+            Command::new("ls")
+                .about("Clones repos")
+                .arg(arg!(<REMOTE> "The remote to clone"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("diff")
+                .about("Compare two commits")
+                .arg(arg!(base: [COMMIT]))
+                .arg(arg!(head: [COMMIT]))
+                .arg(arg!(path: [PATH]).last(true))
+                .arg(
+                    arg!(--color <WHEN>)
+                        .value_parser(["always", "auto", "never"])
+                        .num_args(0..=1)
+                        .require_equals(true)
+                        .default_value("auto")
+                        .default_missing_value("always"),
+                ),
+        )
+        .subcommand(
+            Command::new("push")
+                .about("pushes things")
+                .arg(arg!(<REMOTE> "The remote to target"))
+                .arg_required_else_help(true),
+        )
+        .subcommand(
+            Command::new("add")
+                .about("adds things")
+                .arg_required_else_help(true)
+                .arg(arg!(<PATH> ... "Stuff to add").value_parser(clap::value_parser!(PathBuf))),
+        )
+        .subcommand(
+            Command::new("stash")
+                .args_conflicts_with_subcommands(true)
+                .args(push_args())
+                .subcommand(Command::new("push").args(push_args()))
+                .subcommand(Command::new("pop").arg(arg!([STASH])))
+                .subcommand(Command::new("apply").arg(arg!([STASH]))),
+        )
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use gacm::command::Cli;
-use structopt::StructOpt;
+use gacm::cli;
 
 fn main() {
-    let args = Cli::from_args();
+    let matches = cli().get_matches();
 
-    println!("{:?}", args)
+    match matches.subcommand() {
+        Some(("ls", sub_matches)) => {
+            println!("gacm ls:,{:?}", sub_matches)
+        }
+        Some(("clone", sub_matches)) => {
+            println!(
+                "Cloning {}",
+                sub_matches.get_one::<String>("REMOTE").expect("required")
+            );
+        }
+        other => {
+            println!("{:?}", other)
+        }
+    }
 }
